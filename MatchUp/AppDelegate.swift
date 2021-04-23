564f8a80aa308d6519660c91f57bc1b03bcf0ae6
HideakiYamamoto <poo3gumi@gmail.com>
2021-04-23 11:23:37 +0900

    #1 NoStoryboard用設定変更 & StackView作成

---
+++ MatchUp/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  MatchUp
+//
+//  Created by 山本英明 on 2021/04/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //表示画面
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        //初期画面
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+//    // MARK: UISceneSession Lifecycle
+//
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
+
+
+}
+
---
+++ MatchUp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MatchUp
+//
+//  Created by 山本英明 on 2021/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        //配色
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        let view3 = UIView()
+        view3.backgroundColor = .green
+        
+        //画面構成が複数　→　UIStackView
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        //AutoLayoutに必須
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        //分割
+        stackView.axis = .vertical
+        
+        //大元のViewにstackViewを入れる
+        self.view.addSubview(stackView)
+        
+        //配置(AutoLayout)
+        [
+            view1.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 100),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)]
+            .forEach { $0.isActive = true }
+    }
+}
+
