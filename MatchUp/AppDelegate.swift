6616a7472847cb0b38dd1a029b78417dfb27bc12
HideakiYamamoto <poo3gumi@gmail.com>
2021-04-23 12:07:00 +0900

    #1 個別のビュー(BottomControlView)を作成

--- MatchUp/AppDelegate.swift
+++ MatchUp/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         //初期画面
-        window?.rootViewController = ViewController()
+        window?.rootViewController = HomeViewController()
         
         return true
     }
